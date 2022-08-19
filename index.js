835913a19575c4f78613cacc45ef47ba52767587
Andrew Longhorn <Andrew_Longhorn@TrendMicro.com>
2022-08-19 14:30:36 +1000

    implemnent major functionality

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const axios = require('axios').default;
 const Mustache = require('mustache');
 const Configstore = require('configstore');
 const async = require('async');
+const moment = require('moment');
 const commandLineArgs = require('command-line-args')
 
 const optionDefinitions = [
-  { name: 'silent', alias: 's', type: Boolean }
+    { name: 'silent', alias: 's', type: Boolean }
 ]
-const options = function(){
-  try {
-    return commandLineArgs(optionDefinitions)
-  } catch(err) {
-    console.log('usage:\r\n\tnode index.js [-s|--silent]\r\n');
-    return
-  }
+const options = function () {
+    try {
+        return commandLineArgs(optionDefinitions)
+    } catch (err) {
+        console.log('usage:\r\n\tnode index.js [-s|--silent]\r\n');
+        return
+    }
 }()
-if (!options) {return}
+if (!options) { return }
 
 let conformity = "https://conformity.{{region}}.cloudone.trendmicro.com/api"
 // todo: pre-load from settings?
@@ -24,28 +25,59 @@ let conformity = "https://conformity.{{region}}.cloudone.trendmicro.com/api"
 const config = new Configstore('cloudInventory', {
     region: 'us-1',
     apiKey: null,
-    accounts: {}
+    accounts: {},
+    reports: {},
+    mappings: {}
 });
 
 var region = config.get('region');
 var apiKey = config.get('apiKey');
 var accounts = config.get('accounts');
+var reports = config.get('reports');
+var mappings = config.get('mappings');
+
+function fetchReports(accountId, callback) {
+    const url = Mustache.render(conformity + "/reports", { region: region });
+    axios({
+        method: 'get',
+        url: url,
+        json: true,
+        headers: {
+            Authorization: `ApiKey ${apiKey}`,
+            'Api-Version': 'v1',
+            'Content-Type': 'application/vnd.api+json'
+        },
+        params: {
+            accountId: accountId
+        }
+    })
+        .then(function (response) {
+            if (!response.status || (response.status !== 200) || !response.data || !response.data.data) {
+                return callback(response.status ? response.status : "failed");
+            }
+            callback(null, response.data.data);
+        })
+        .catch(callback);
+};
 
 function fetchData(commandInstance, callback) {
-    const proxy = { proxy: {
-        protocol: 'https',
-        host: '127.0.0.1',
-        port: 9000,
-        auth: {
-          username: 'mikeymike',
-          password: 'rapunz3l'
+
+    // todo: implement optional proxy functionality
+    const proxy = {
+        proxy: {
+            protocol: 'https',
+            host: '127.0.0.1',
+            port: 9000,
+            auth: {
+                username: 'mikeymike',
+                password: 'rapunz3l'
+            }
         }
-      }
     };
-    
+
     async.auto({
-        "accounts" : function(callback) {
-            commandInstance.log('fetching accounts...');
+        "accounts": function (callback) {
+            commandInstance.log('\nfetching accounts...');
             const url = Mustache.render(conformity + "/accounts", { region: region });
             axios({
                 method: 'get',
@@ -53,101 +85,271 @@ function fetchData(commandInstance, callback) {
                 json: true,
                 headers: {
                     Authorization: `ApiKey ${apiKey}`,
-                    'Api-Version':  'v1',
+                    'Api-Version': 'v1',
                     'Content-Type': 'application/vnd.api+json'
                 }
             })
-            .then(function(response) {
-              if (!response.status || (response.status !== 200) || !response.data || !response.data.data) {
-                return callback(response.status ? response.status : "failed");
-              }
-              accounts = response.data.data.reduce(function(memo, account) {
-                return Object.assign({}, memo, {
-                  [account.id]: account
+                .then(function (response) {
+                    if (!response.status || (response.status !== 200) || !response.data || !response.data.data) {
+                        return callback(response.status ? response.status : "failed");
+                    }
+                    accounts = response.data.data.reduce(function (memo, account) {
+                        return Object.assign({}, memo, {
+                            [account.id]: account
+                        })
+                    }, {});
+                    config.set('accounts', accounts);
+                    commandInstance.log(`Found ${Object.keys(accounts).length} accounts:`);
+                    Object.values(accounts)
+                        .sort(function (a, b) { a.attributes.name.localeCompare(b.attributes.name) })
+                        .forEach(function (account) {
+                            commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment})`);
+                        });
+                    callback(null, accounts);
                 })
-              }, {});
-              config.set('accounts', accounts);
-              commandInstance.log(`Found ${Object.keys(accounts).length} accounts:`);
-              Object.values(accounts)
-                .sort(function(a, b) { a.attributes.name < b.attributes.name})
-                .forEach(function(account) {
-                  commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment})`);
-                });
-              callback(null, accounts);
-            })
-            .catch(callback);
+                .catch(callback);
         },
 
-        'reports' : ['accounts', function(data, callback) {
-            commandInstance.log(data.accounts);
-            config.set('accounts', data.accounts);
-            commandInstance.log('fetching reports...')
-            callback()
+        'reports': ['accounts', function (data, callback) {
+            commandInstance.log('\nfetching reports...');
+
+            async.reduce(Object.values(accounts).sort(function (a, b) {
+                a.attributes.name.localeCompare(b.attributes.name)
+            }), {}, function(memo, account, callback) {
+                fetchReports(account.id, function(err, data) {
+                        if (err) { return callback(err); }
+
+                        reportInfo = data.reduce(function(memo, report) {
+                            const attr = report.attributes;
+                            const id = attr['report-config-id'];
+                            const last = memo[id] || attr;
+                            return Object.assign({}, memo, {
+                                [id]: (attr['created-date'] > last['created-date'] ? attr : last)
+                            });
+                        }, {});
+
+                        commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment}):`);
+                        Object.values(reportInfo).forEach(function(report) {
+                            const date = moment(report['created-date']);
+                            commandInstance.log(`       ${report.title}${report.daily ? ' (daily)' : ''} latest is ${date.fromNow()}`);
+                        });
+                        callback(null, Object.assign({}, memo, {
+                            [account.id]: reportInfo
+                        }));
+                    });
+            }, function(error, data) {
+                if (error) { return callback(error); }
+                reports = data;
+                config.set('reports', reports);
+                // todo: clean up broken mappings? or publish warnings? 
+                callback();
+            });
         }]
     }, callback);
 }
 
+function downloadCSV(reportInfo, callback) {
+    const url = reportInfo.included.reduce(function(memo, entry) {
+        if (entry.type === 'CSV') {
+            return entry["report-download-endpoint"];
+        }
+        return memo;
+    }, null);
 
-console.log(options);
+    if (!url) { return callback(); } //- silently fail for now
 
+    axios.get(url,
+    // {
+    //   headers: {
+    //     "User-Agent":
+    //       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    //   }
+    // }
+    ).then((res) => {
+        console.log(res);
+        callback('1,2,3');
+    });
+}
+
+function generateReport(commandInstance, callback) {
+    const accountIds = Object.keys(mappings);
+
+    async.auto({
+        // get the latest reports in parallel
+        reports: function(callback) {
+            async.map(accountIds, function(accountId, callback) {
+                fetchReports(accountId, function(err, data) {
+                    if (err) { return callback(err); }
+
+                    reportInfo = data.reduce(function(memo, report) {
+                        const attr = report.attributes;
+                        const id = attr['report-config-id'];
+                        const last = memo[id] || attr;
+                        return Object.assign({}, memo, {
+                            [id]: (attr['created-date'] > last['created-date'] ? attr : last)
+                        });
+                    }, {});
+                    
+                    // here we have the exact report we need to use for this account
+                    // fetch the data
+                    downloadCSV(reportInfo, function(err, csv) {
+                        callback(err, {
+                            accountId : accountId,
+                            reportInfo : reportInfo,
+                            csv : csv
+                        });
+                    });
+                });
+            }, callback);
+        },
+
+        csv: ['reports', function(data, callback) {
+            // massage all the data to arrive at the final csv output
+            callback(null, 'fred');
+        }]
+    }, function(err, data) {
+        console.log(err, data);
+        if (callback) { callback(err, data && data.csv); }
+    });
+}
+
+if (options.silent) {
+    return generateReport({
+        log: (err) => {console.error(err)}
+    });
+}
 
 vorpal
-  .command('init <region> <apiKey>', 'initialise cloudInventory.')
-  .action(function(args, callback) {
-    region = args.region;
-    apiKey = args.apiKey;
-    config.set('region', region);
-    config.set('apiKey', apiKey);
-    callback();
-  });
+    .command('init <region> <apiKey>', 'initialise cloudInventory.Region is one of [ us-1 | etc... ]')
+    .action(function (args, callback) {
+        region = args.region;
+        apiKey = args.apiKey;
+        config.set('region', region);
+        config.set('apiKey', apiKey);
+        callback();
+    });
 
 vorpal
-  .command('fetch', 'fetch (update) all accounts and reports.')
-  .action(function(args, callback) {
-    fetchData(this, callback);
-  });
-
-  vorpal
-  .command('accounts', 'list all accounts.')
-  .action(function(args, callback) {
-    const commandInstance = this;
-    if (Object.keys(accounts).length < 1) {
-      commandInstance.log("No accounts, try running 'fetch'.");
-      return;
-    }
-    Object.values(accounts)
-      .sort(function(a, b) { a.attributes.name < b.attributes.name})
-      .forEach(function(account) {
-        commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment})`);
-      });
-      callback();
-  });
+    .command('fetch', 'fetch (update) all accounts and reports.')
+    .action(function (args, callback) {
+        fetchData(this, callback);
+    });
 
 vorpal
-  .command('proxy <protocol> <host> <port> <username> <password>', 'configure proxy.')
-  .action(function(args, callback) {
-    proxyProtocol = args.protocol;
-    proxyHost = args.host;
-    proxyPort = args.port;
-    proxyUsername = args.username;
-    proxyPassword = args.password;
-    apiKey = args.apiKey;
-    config.set('proxyProtocol', proxyProtocol);
-    config.set('proxyHost', proxyHost);
-    config.set('proxyPort', proxyPort);
-    config.set('proxyUsername', proxyUsername);
-    config.set('proxyPassword', proxyPassword);
-    fetchData(this, callback);
-  });
+    .command('accounts', 'list all accounts.')
+    .action(function (args, callback) {
+        listAccounts(this, callback);
+    });
 
-
+// todo
 // vorpal
-//   .command('accounts', 'Retrieves updates your list of accounts.')
-//   .action(function(args, callback) {
-//     this.log('bar');
-//     callback();
-//   });
- 
+//     .command('proxy <protocol> <host> <port> <username> <password>', 'configure proxy.')
+//     .action(function (args, callback) {
+//         proxyProtocol = args.protocol;
+//         proxyHost = args.host;
+//         proxyPort = args.port;
+//         proxyUsername = args.username;
+//         proxyPassword = args.password;
+//         apiKey = args.apiKey;
+//         config.set('proxyProtocol', proxyProtocol);
+//         config.set('proxyHost', proxyHost);
+//         config.set('proxyPort', proxyPort);
+//         config.set('proxyUsername', proxyUsername);
+//         config.set('proxyPassword', proxyPassword);
+//         fetchData(this, callback);
+//     });
+
+function listAccounts(commandInstance, callback) {
+    if (Object.keys(accounts).length < 1) {
+        commandInstance.log("No accounts, try running 'fetch'.");
+        return;
+    }
+    console.log(mappings);
+    Object.values(accounts)
+        .sort(function (a, b) { a.attributes.name < b.attributes.name })
+        .forEach(function (account) {
+            const reportId = mappings[account.id];
+            const accountReports = account.id && reports[account.id];
+            const report = accountReports && accountReports[reportId];
+            const display = report ? `${report.title}${report.daily ? ' (daily)' : ''}` : `no report assigned, please use "set ${account.attributes.name}" command`;
+        commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment}) : ${display}`);
+        });
+    if (callback) { callback(); }
+}
+
+function showConfig(commandInstance, callback) {
+    if (region) {
+        commandInstance.log(`\nusing region: ${region}`);
+        listAccounts(commandInstance, function(err) {
+            if (err) {
+                return (callback || (() => {}))(err);
+            }
+        });
+    } else {
+        commandInstance.log(`\nno region set, run "init" command first`);
+    }
+    commandInstance.log('');
+    if (callback) { callback() };
+}
+
 vorpal
-  .delimiter('cloudInventory$')
-  .show();
+    .command('config', 'Displays current configuration.')
+    .action(function(args, callback) {
+        showConfig(this, callback);
+    });
+
+vorpal
+    .command('set <account> <report>', 'assign which report to use for the given account.')
+    .autocomplete(() => {
+        const args = vorpal.ui.input().split(' ');
+        if (args.length > 3) { return []; }
+        if (args.length > 2) {
+            const id = Object.values(accounts).reduce(function(memo, account) {
+                if (account.attributes.name == args[1]) {
+                    return account.id;
+                }
+                return memo;
+            }, null);
+            if (!id) { return null; }
+            //console.log(id, reports);
+            return Object.values(reports[id] || {}).map((report) => `"${report.title}"`).sort();
+        }
+        return Object.values(accounts).map((account) => account.attributes.name).sort();
+    })  
+    .action(function(args, callback) {
+        const commandInstance = this;
+        //commandInstance.log(args);
+        const account = Object.values(accounts).reduce(function(memo, account) {
+            if (account.attributes.name == args.account) {
+                return account;
+            }
+            return memo;
+        }, null);
+        if (!account) { return callback(new Error("Account not found")); }
+        const report = Object.values(reports[account.id] || {}).reduce(function(memo, report) {
+            if (report.title == args.report) {
+                return report;
+            }
+            return memo;
+        }, null);
+        if (!report) { return callback(new Error("Report not found")); }
+
+        mappings[account.id] = report['report-config-id'];
+        config.set('mappings', mappings);
+        const display = report ? `${report.title}${report.daily ? ' (daily)' : ''}` : `no report assigned, please use "set ${account.attributes.name}" command`;
+        commandInstance.log(`   ${account.attributes.name} (${account.attributes.environment}) : ${display}`);
+        callback();
+    });
+
+vorpal
+    .command('generate [filename]', 'Generate the summary report in csv.')
+    .action(function(args, callback) {
+        generateReport(this, callback);
+    });
+
+  // first display configuration
+showConfig(vorpal);
+
+vorpal
+    .delimiter('cloudInventory$')
+    .show();
